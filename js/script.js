2b7bca472734f029966ea6f5b070991357a8380a
Fredrik <fredrikf2000@gmail.com>
2020-07-21 15:04:50 +0200

    fetched content and added some styling

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const url = "http://fedup.fredrikfordelsen.no/wp-json/wp/v2/posts";
+const postContainer = document.querySelector(".myPosts");
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        data.map( post => {
+            const innerContent = 
+            `
+            <li>
+                <h1>${post.title.rendered}</h1>
+                <h3>Author: ${post.author}</h3>
+                ${post.excerpt.rendered}
+                <a href="${post.link}">Read More</a>
+                <p>Published on: ${post.date}</p>
+            </li>
+            `
+            postContainer.innerHTML += innerContent;
+        })
+    });
